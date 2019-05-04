2428a86814c89690d52e0d619d0acccf0be57481
Dewi Fidrianingrum <dewifidrianingrum@gmail.com>
2019-05-05 01:51:19 +0700

    Login Google API, Profil Akun Sign Out

--- GudangBidan/app/src/main/java/com/example/gudangbidan/Login.java
+++ GudangBidan/app/src/main/java/com/example/gudangbidan/Login.java
@@ -1,13 +1,23 @@
 package com.example.gudangbidan;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class Login extends AppCompatActivity {
     EditText mTextUsername;
@@ -15,6 +25,10 @@ public class Login extends AppCompatActivity {
     Button mButtonLogin;
     TextView mTextViewRegister;
     DatabaseHelper db;
+    private static final String TAG = "AndroidClarified";
+    private GoogleSignInClient googleSignInClient;
+    ImageButton googleSignInButton;
+    private GoogleSignInAccount googleSignInAccount;
 
 
     @Override
@@ -55,6 +69,62 @@ public class Login extends AppCompatActivity {
             }
         });//onClick
 
+        googleSignInButton = findViewById(R.id.imagebuttonGoogle);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getResources().getString(R.string.idToken))
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+        googleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = googleSignInClient.getSignInIntent();
+                signInIntent.putExtra(Profil.GOOGLE_ACCOUNT, googleSignInAccount);
+                startActivityForResult(signInIntent, 101);
+            }
+        });//onClick
+
+
+
     }//onCreate
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK)
+            switch (requestCode) {
+                case 101:
+                    try {
+
+                        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                        GoogleSignInAccount account = task.getResult(ApiException.class);
+                        String idToken = account.getIdToken();
+                        onLoggedIn(account);
+                    } catch (ApiException e) {
+
+                        Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+                    }
+                    break;
+            }
+    }//onActivity
+
+    private void onLoggedIn(GoogleSignInAccount googleSignInAccount) {
+        Intent berandaintent = new Intent(this, BerandaLogin.class);
+        startActivity(berandaintent);
+    }//onLoggedIn
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        GoogleSignInAccount alreadyloggedAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if (alreadyloggedAccount != null) {
+            Toast.makeText(this, "Sudah Masuk", Toast.LENGTH_SHORT).show();
+            onLoggedIn(alreadyloggedAccount);
+        } else {
+            Log.d(TAG, "Belum Masuk");
+        }
+    }
+
+
+
 
 }//end
---
+++ GudangBidan/app/src/main/java/com/example/gudangbidan/Profil.java
@@ -0,0 +1,62 @@
+package com.example.gudangbidan;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+public class Profil extends AppCompatActivity {
+    public static final String GOOGLE_ACCOUNT = "google_account";
+    private TextView profileName, profileEmail;
+    private ImageView profileImage;
+    private Button signOut;
+    private GoogleSignInClient googleSignInClient;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil);
+        profileName = findViewById(R.id.profile_text);
+        profileEmail = findViewById(R.id.profile_email);
+        profileImage = findViewById(R.id.profile_image);
+        signOut=findViewById(R.id.buttonSignout);
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent intent=new Intent(Profil.this,MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                    }
+                });
+            }
+        });
+    }
+
+    private void setDataOnView() {
+        GoogleSignInAccount googleSignInAccount = getIntent().getParcelableExtra(GOOGLE_ACCOUNT);
+        //Picasso.get().load(googleSignInAccount.getPhotoUrl()).centerInside().fit().into(profileImage);
+        profileName.setText(googleSignInAccount.getDisplayName());
+        profileEmail.setText(googleSignInAccount.getEmail());
+    }
+
+
+
+
+}
